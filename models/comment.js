ac0d23844caf55826f46a3470acca034dc81e68a
Md. Zahidur Rahman <45038726+zahidtdx61@users.noreply.github.com>
2024-06-12 12:04:53 +0600

    feat: Add Comment model with required fields and default values

---
+++ models/comment.js
@@ -0,0 +1,30 @@
+const { Mongoose } = require("../configs");
+
+const commentSchema = new Mongoose.Schema({
+  comment: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  userId: {
+    type: Schema.Types.ObjectId,
+    required: true,
+    ref: "User",
+  },
+  postId: {
+    type: Schema.Types.ObjectId,
+    required: true,
+    ref: "Post",
+  },
+  status: {
+    type: String,
+    required: true,
+    default: "active",
+  },
+});
+
+const Comment = Mongoose.model("Comment", commentSchema);
+module.exports = Comment;
