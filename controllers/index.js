15908e9655f87b44e687bc4e1182fe8dbacef67f
Md. Zahidur Rahman <45038726+zahidtdx61@users.noreply.github.com>
2024-06-18 13:09:35 +0600

    feat: Add PaymentController for managing payments

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,8 @@
+const Payment = require("../models/payment");
+
 module.exports = {
   AdminController: require("./admin-controller"),
   UserController: require("./user-controller"),
   PostController: require("./post-controller"),
+  PaymentController: require("./payment-controller"),
 };
